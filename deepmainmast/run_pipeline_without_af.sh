e780706c035f013e6ffdceeeb3b8a4552f4a79cc
devashishprasad <devashishkprasad@gmail.com>
2022-11-04 16:55:04 -0400

    added run_pipeline sh scripts

---
+++ deepmainmast/run_pipeline_without_af.sh
@@ -0,0 +1,21 @@
+ID=$1
+
+./0_setup.sh $ID
+
+./a_cmd_emap2secplus.sh $ID
+
+./b_cmd_node_long.sh $ID
+
+./c_cmd_path.sh $ID
+
+./d_cmd_modeling.sh $ID
+
+./g_cmd_combine_itr.sh $ID
+
+./h_cmd_rosettaCM_setup.sh $ID
+
+./i_cmd_rosettaCM.sh $ID
+
+./j_cmd_daq_cm.sh $ID
+
+./k_cmd_dotscore.sh $ID
