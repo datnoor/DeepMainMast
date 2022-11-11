3d87731405379471b1212266df647c5a615d62d5
devashishprasad <devashishkprasad@gmail.com>
2022-11-11 15:57:51 -0500

    emap2secplus filename change

--- deepmainmast/a_cmd_emap2secplus.sh
+++ deepmainmast/a_cmd_emap2secplus.sh
@@ -1,7 +1,7 @@
 ID=$1
 CONTOUR=$2
 
-python3 emap2secplus/main.py --mode=0 -F=input/$ID/input.mrc -M=emap2secplus/best_model/Unet_Protein_Atom.pth.tar --contour=$CONTOUR --gpu=0 --batch_size=16 --type=0
+python3 emap2secplusdmm/main.py --mode=0 -F=input/$ID/input.mrc -M=emap2secplus/best_model/Unet_Protein_Atom.pth.tar --contour=$CONTOUR --gpu=0 --batch_size=16 --type=0
 
-python3 emap2secplus/main.py --mode=0 -F=input/$ID/input.mrc -M=emap2secplus/best_model/Unet_Protein_AA.pth.tar --contour=$CONTOUR --gpu=0 --batch_size=16 --type=1
+python3 emap2secplusdmm/main.py --mode=0 -F=input/$ID/input.mrc -M=emap2secplus/best_model/Unet_Protein_AA.pth.tar --contour=$CONTOUR --gpu=0 --batch_size=16 --type=1
 
