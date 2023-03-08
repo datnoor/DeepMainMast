05497dc560da219a7ae16486250a36c9dd9f4149
DevashishPrasad <devashishkprasad@gmail.com>
2023-03-07 21:39:22 -0500

    fixed chk -gt 100 bug

--- dmmmultichain/h1_cmd_rosettaCM_setup_rechain.sh
+++ dmmmultichain/h1_cmd_rosettaCM_setup_rechain.sh
@@ -11,7 +11,7 @@ do
 	tag=`basename $model .pdb`
 	dname=results/$ID/CM_$tag
 	chk=`wc -l $model|perl -alne 'print $F[0]'`
-	if [ -e $model ] && [ $chk -gt 100 ] && [ ! -e $dname ];then
+	if [ -e $model ] && [ $chk -gt 10 ] && [ ! -e $dname ];then
 		##Split Chains and PULCHRA
 		##Make Alignment and threading files for Rosetta		
 		python3 $PG $seq $model $DATA --OutPath=$dname --XMLPath=$xpath
